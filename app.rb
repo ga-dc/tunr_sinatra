2694c5cccfb61ff4cf8abaafa632d60e1fe50438
Zaneh Williams <zanehmw@gmail.com>
2016-04-14 11:17:27 -0400

    created an edit feature

--- app.rb
+++ app.rb
@@ -39,3 +39,14 @@ get '/artists/:id' do
   @artist = Artist.find(params[:id])
   erb :"artists/show"
 end
+
+get "/artists/:id/edit" do
+  @artist = Artist.find(params[:id])
+  erb(:"artists/edit")
+end
+
+put '/artists/:id' do
+  @artist = Artist.find(params[:id])
+  @artist.update(params[:artist])
+  redirect("/artists/#{@artist.id}")
+end
