4faeacec86dd9fed4862001dd7ddeeeae82ed5b1
Zaneh Williams <zanehmw@gmail.com>
2016-04-14 11:02:43 -0400

    built a  feature that creates a new artist in the database

---
+++ app.rb
@@ -0,0 +1,41 @@
+require 'bundler/setup'
+require 'active_record'
+require 'pg'
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+# Load the file to connect to the DB
+require_relative 'db/connection'
+
+# Load models
+require_relative 'models/artist'
+require_relative 'models/song'
+
+# artist = instructor
+# song = student
+
+get '/artists' do
+  @artists = Artist.all
+  erb :"artists/index"
+end
+
+get '/songs' do
+  @songs = Song.all
+  erb :"songs/index"
+end
+
+post '/artists' do
+  @artist = Artist.create(params[:artist])
+  redirect "/artists/#{@artist.id}"
+end
+
+get '/artists/new' do
+  erb :"artists/new"
+end
+
+get '/artists/:id' do
+  @artist = Artist.find(params[:id])
+  erb :"artists/show"
+end
